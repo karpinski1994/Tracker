4727a882efa58ca66d2e75386a794fc8fa0fe3f4
Patryk <patryk.karpinski@outlook.com>
2018-10-28 11:52:35 +0100

    almost finished DIContainer

--- frontend/src/di/DIContainer.ts
+++ frontend/src/di/DIContainer.ts
@@ -2,16 +2,16 @@ const areObjEqual = require('../utils/areObjEqual');
 
 interface Container {
   queue: Array<object>;
-  instances: Array<object>;
   solved: Array<object>;
+  instances: Array<object>;
 }
 
 class Container {
 
   constructor() {
     this.queue = [];
-    this.classes = [];
     this.solved = [];
+    this.instances = [];
   }
 
   register(target: any, depName?: string) {
@@ -40,22 +40,67 @@ class Container {
   // }
 
   initiate() {
-    this.queue.forEach(item => {
-      if(item.deps.length === 0) {
-        this.classes.push(item.class);
-        const fIndex = this.queue.findIndex(i => i === item);
-        if(fIndex !== -1){
-          this.queue.splice(fIndex, 1);
+    //pozniej na while queue lenght > 0
+    for (let i = 0; i < 10; i++) {
+      this.queue.forEach(item => {
+        console.log('ITEM:', item)
+        if(item.deps.length === 0) {
+          const instI = this.instances.findIndex(i => i instanceof item.class);
+          if (instI === -1 ){
+            this.instances.push(new item.class());
+            this.solved.push(item);
+            const fIndex = this.queue.findIndex(i => i === item);
+            if (fIndex !== -1) {
+              this.queue.splice(fIndex, 1);
+            }
+          } else {
+            this.solved.push(item);
+            const fIndex = this.queue.findIndex(i => i === item);
+            if (fIndex !== -1) {
+              this.queue.splice(fIndex, 1);
+            }
+          }
+        } else {
+          item.deps.forEach(depName => {
+            const fDepInstanceIndex = this.solved.findIndex(s => s.name === depName);
+            // sprawdzamy zaleznosc czy ma instancje
+            // jesli ma
+            if (fDepInstanceIndex !== -1) {
+              console.log(console.log('instance to inject: ', this.instances[fDepInstanceIndex]));
+              const instanceToInject = this.instances[fDepInstanceIndex];
+              console.log("INSTANCE TO INJECT:", instanceToInject)
+              // powinnismy sprawdzac czy ta zaleznosc ma instancje
+              // sprawdzamy czy rodzic ma instancje
+              const itemInstanceIndex = this.instances.findIndex(i => i instanceof item.class);
+              console.log('itemInstanceIndex', itemInstanceIndex);
+              // jesli instancja jest to pushujemy nowa zaleznosc
+              if (itemInstanceIndex !== -1) {
+                console.log("UWAGA!")
+                console.log('INSTANCJA: ', this.instances[itemInstanceIndex]);
+                console.log('UWAGA!');
+                console.log('ITEM PRZED DEPENDENCY: ', item);
+                const rDepI = item.deps.findIndex(d => d.name === depName);
+                const removedDep = item.deps.splice(rDepI, 1);
+                console.log('UWAGA!');
+                console.log('ITEM PO DODANIU DEPENDENCY: ', item);
+               // jesli nie ma tworzymy instancje 
+              } else {
+                const rDepI = item.deps.findIndex(d => d.name === depName);
+                const removedDep = item.deps.splice(rDepI, 1);
+                this.instances.push(new item.class(instanceToInject));
+              }
+              
+              // console.log('----------------------------')
+              
+            }
+          });
         }
-      } else {
-        item.deps.forEach(dep => {
-          console.log('dep', dep);
-          this.classes.forEach(c => console.log('d: ',c.name))
-        });
-      }
-    })
-    console.log('this.classes', this.classes);
-    console.log('this.queue', this.queue);
+      })
+    }
+    // console.log('----------------------------')
+    console.log('this.solved', this.solved);
+    console.log('this.instances', this.instances);
+    // console.log('this.queue', this.queue);
   }
 
   instantiate(element: any) {
--- frontend/src/services/helper.service.ts
+++ frontend/src/services/helper.service.ts
@@ -2,7 +2,7 @@ import { Inject } from '../di/DIContainer';
 
 @Inject('utilsService')
 export class helperService {
-  constructor() {
+  constructor(private utS) {
     console.log('tworze helperService');
   }
 }
